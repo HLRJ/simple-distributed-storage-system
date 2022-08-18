9ce3e19584289b4ba4a171a3b86e8eb57d5a2675
Zhenxiang Wang <befun@vip.qq.com>
2022-08-18 16:17:28 +0800

    Delete scripts directory
    存在相同功能的代码

--- scripts/proto-gen-go.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
-protoc -I ../src/protos ../src/protos/namenode.proto --go_out=../src/protos --go-grpc_out=../src/protos
-protoc -I ../src/protos ../src/protos/datanode.proto --go_out=../src/protos --go-grpc_out=../src/protos
