771fdfe63ab25bb40e3277fa9040dfbc5c3ed885
Zhenxiang Wang <befun@vip.qq.com>
2022-08-19 09:27:52 +0800

    feat: ctl basic api

--- cmd/ctl/commands/put_object.go
+++ cmd/ctl/commands/put_object.go
@@ -5,18 +5,20 @@ import (
 	"simple-distributed-storage-system/src/client"
 )
 
+// 输入 本地文件路径 local _ file _ path 远程文件路径 remote _ file _ path
+// 输出 是否成功result
 var putObjectCmd = &cobra.Command{
-	Use:   "put_object [meta server addrs] [local file path] [bucket id]",
+	Use:   "Put [local _ file _ path] [remote _ file _ path]",
 	Short: "put object to remote SDSS cluster",
-	Long:  ``,
+	Long:  `将本地文件上传分布式文件存储系统`,
 	Run: func(cmd *cobra.Command, args []string) {
-		SDSS_Cli := client.NewClient() //需增加namenode的地址
+		SDSS_Cli := client.NewClient()
 		//objectId, err := SDSS_Cli.Put(args[1], args[2]) //需增加错误处理
 		//if err != nil {
 		//	fmt.Printf("%s\n", err.Error())
 		//}
 		//fmt.Printf("put object return %s\n", objectId)
-		SDSS_Cli.Put(args[1], args[2])
+		SDSS_Cli.Put(args[0], args[1])
 	},
 }
 
