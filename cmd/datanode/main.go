e174b5886447047d1a46da448cd9ef60adc69a70
jinbo-self <108263790+jinbo-self@users.noreply.github.com>
2022-08-19 10:03:31 +0800

    Revert "Dev"

--- cmd/datanode/main.go
+++ cmd/datanode/main.go
@@ -1,38 +1,23 @@
 package main
 
 import (
-	"flag"
+	"log"
 	"net"
+	"simple-distributed-storage-system/src/consts"
 	"simple-distributed-storage-system/src/datanode"
 	"simple-distributed-storage-system/src/protos"
-	"strings"
-
-	log "github.com/sirupsen/logrus"
 
 	"google.golang.org/grpc"
 )
 
-var nodeId = flag.Int("id", 0, "input this block server node id")
-var nodeAddress = flag.String("address", "127.0.0.1:7088,127.0.0.1:7089,127.0.0.1:7090", "node addrs")
-
 func main() {
-
-	blockSvrNodesMap := make(map[int]string)
-	nodeAddrsArr := strings.Split(*nodeAddress, ",")
-	for i, addr := range nodeAddrsArr {
-		blockSvrNodesMap[i] = addr
-	}
-	log.Infof("start to listen: %v", blockSvrNodesMap[*nodeId])
-	lis, err := net.Listen("tcp", blockSvrNodesMap[*nodeId])
+	lis, err := net.Listen("tcp", consts.DataNodeServerAddr)
 	if err != nil {
-		log.Infof("failed to listen: %v", err)
+		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
+
 	DataServer := datanode.MakeServer()
-	protos.RegisterDataNodeServer(s, DataServer)
-	errServe := s.Serve(lis)
-	if errServe != nil {
-		log.Panic(errServe)
-	}
-	log.Info("successfully start nodeServer")
+	protos.RegisterDataserverServer(s, DataServer)
+	s.Serve(lis)
 }
