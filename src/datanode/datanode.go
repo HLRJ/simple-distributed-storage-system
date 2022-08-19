eb671c4a84a7ecb51cfbe9214979ed115726385c
jinbo-self <108263790+jinbo-self@users.noreply.github.com>
2022-08-18 19:28:25 -0700

    Add files via upload

--- src/datanode/datanode.go
+++ src/datanode/datanode.go
@@ -2,91 +2,76 @@ package datanode
 
 import (
 	"context"
-	log "github.com/sirupsen/logrus"
-	"github.com/syndtr/goleveldb/leveldb"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-	"net"
 	"os"
-	"simple-distributed-storage-system/src/consts"
 	"simple-distributed-storage-system/src/protos"
+
+	log "github.com/sirupsen/logrus"
 )
 
-type dataNodeServer struct {
-	protos.DataNodeServer
+const LocalFileSystemRoot string = "/tmp/gfs/chunks/"
 
-	blockSize uint64
-	db        *leveldb.DB
-	addr      string
+type DataServer struct {
+	protos.UnimplementedDataNodeServer
 }
 
-func (s *dataNodeServer) Read(ctx context.Context, in *protos.ReadRequest) (*protos.ReadReply, error) {
-	log.Infof("datanode server %v read %v", s.addr, in.Uuid)
-	data, err := s.db.Get(in.Uuid, nil)
-	if err != nil {
-		return nil, err
+//读文件
+func (c *DataServer) Read(ctx context.Context, req *protos.ReadRequest) (*protos.ReadReply, error) {
+	uuid := string(req.GetUuid())
+	log.Infof("start to read the file: %v", uuid)
+	filepath := LocalFileSystemRoot + uuid
+
+	fileStream, errStream := os.ReadFile(filepath)
+	if errStream != nil {
+		log.Panic("failed to open a file: %v", errStream)
+		return &protos.ReadReply{Data: []byte("failed to read a fileStream")}, errStream
 	}
-	return &protos.ReadReply{Data: data}, nil
+	log.Infof("successfully read the file: %v", uuid)
+	return &protos.ReadReply{Data: fileStream}, nil
 }
 
-func (s *dataNodeServer) Write(ctx context.Context, in *protos.WriteRequest) (*protos.WriteReply, error) {
-	log.Infof("datanode server %v write %v - %v", s.addr, in.Uuid, in.Data)
-	err := s.db.Put(in.Uuid, in.Data, nil)
-	if err != nil {
-		return nil, err
+//写入文件到磁盘
+func (c *DataServer) Write(ctx context.Context, req *protos.WriteRequest) (*protos.WriteReply, error) {
+	uuid := string(req.GetUuid())
+	data := req.Data
+	log.Infof("start to write the file:%v", uuid)
+	filepath := LocalFileSystemRoot + uuid
+
+	fileStream, errStream := os.Create(filepath)
+	if errStream != nil {
+		log.Panic("failed to create a file:%v", errStream)
 	}
+	defer fileStream.Close()
+
+	_, errWrite := fileStream.Write(data)
+	if errWrite != nil {
+		log.Panic("failed to write the file:%v", uuid)
+	}
+	log.Infof("successfully write the file: %v", uuid)
 	return &protos.WriteReply{}, nil
 }
 
-func NewDataNodeServer(address string) *dataNodeServer {
-	db, err := leveldb.OpenFile(os.TempDir()+"/"+address, nil)
-	if err != nil {
-		log.Panic(err)
-	}
-	return &dataNodeServer{
-		addr: address,
-		db:   db,
-	}
+//返回心跳包
+func (c *DataServer) HeartBeat(ctx context.Context, in *protos.HeartBeatRequest) (*protos.HeartBeatReply, error) {
+	return &protos.HeartBeatReply{}, nil
 }
 
-func (s *dataNodeServer) Setup() {
-	// setup datanode server
-	log.Infof("starting datanode server at %v", s.addr)
-	listener, err := net.Listen("tcp", s.addr)
-	if err != nil {
-		log.Panic(err)
+//删除文件
+func (c *DataServer) Remove(ctx context.Context, req *protos.RemoveRequest) (*protos.RemoveReply, error) {
+	uuid := string(req.GetUuid())
+	log.Infof("start to remove the file:%v", uuid)
+	filepath := LocalFileSystemRoot + uuid
+
+	errRemove := os.Remove(filepath)
+	if errRemove != nil {
+		log.Panic("failed to remove the file:%v", uuid)
+	} else {
+		log.Infof("successfully remove the file:%v", uuid)
 	}
-	server := grpc.NewServer()
-	protos.RegisterDataNodeServer(server, s)
+	return &protos.RemoveReply{}, nil
+}
 
-	go func() {
-		err = server.Serve(listener)
-		if err != nil {
-			log.Panic(err)
-		}
-	}()
-
-	// connect to namenode
-	conn, err := grpc.Dial(consts.NameNodeServerAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Panic(err)
-	}
-	defer func(conn *grpc.ClientConn) {
-		err := conn.Close()
-		if err != nil {
-			log.Panic(err)
-		}
-	}(conn)
-
-	client := protos.NewNameNodeClient(conn)
-	reply, err := client.RegisterDataNode(context.Background(), &protos.RegisterDataNodeRequest{Address: s.addr})
-	if err != nil {
-		log.Panic(err)
-	}
-
-	// set block size
-	s.blockSize = reply.BlockSize
-
-	// blocked here
-	select {}
+//将DataServer结构体供外部调用
+func MakeServer() *DataServer {
+	Server := &DataServer{}
+	return Server
 }
