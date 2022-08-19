83669ea62c9cf29520487a03972d98541c5453e9
jinbo-self <108263790+jinbo-self@users.noreply.github.com>
2022-08-18 19:27:44 -0700

    Add files via upload

--- cmd/datanode/main.go
+++ cmd/datanode/main.go
@@ -2,12 +2,31 @@ package main
 
 import (
 	"flag"
+	"net"
 	"simple-distributed-storage-system/src/datanode"
+	"simple-distributed-storage-system/src/protos"
+
+	log "github.com/sirupsen/logrus"
+
+	"google.golang.org/grpc"
 )
 
 var dataNodeServerAddr = flag.String("addr", "localhost:9000", "input this datanode address")
 
 func main() {
+
 	flag.Parse()
-	datanode.NewDataNodeServer(*dataNodeServerAddr).Setup()
+
+	log.Infof("start to listen: %v", *dataNodeServerAddr)
+	lis, err := net.Listen("tcp", *dataNodeServerAddr)
+	if err != nil {
+		log.Infof("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	DataServer := datanode.MakeServer()
+	protos.RegisterDataNodeServer(s, DataServer)
+	errServe := s.Serve(lis)
+	if errServe != nil {
+		log.Panic(errServe)
+	}
 }
